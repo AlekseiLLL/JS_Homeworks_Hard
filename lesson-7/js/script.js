cb476bc45de19ab5629c7cdfdadfde516d5e7349
Aleksei <aleksei.lokhno@ukr.net>
2018-04-03 18:29:21 +0300

    Homework 7 ver.1.0

---
+++ lesson-7/js/script.js
@@ -0,0 +1,20 @@
+// RequestAnimationFrame 
+// Нужен для группировки всех перерисовок в одну и для запуска их всех вместе.
+
+function animate(options) {
+	// Время, прошедшее с начала загрузки страницы
+	let start = performance.now();
+	// Получем параметр 
+	requestAnimationFrame(function animate(time) {
+    	// Общее время, которое должна длиться анимация, от 0 до 1
+    	let timeFraction = (time - start) / options.duration;
+    	if (timeFraction > 1) timeFraction = 1;
+    	// Получаем текущее состояние анимации
+    	let progress = options.timing(timeFraction)
+    	// Рисуем
+    	options.draw(progress);
+    	if (timeFraction < 1) {
+    		requestAnimationFrame(animate);
+    	}
+	});
+}
