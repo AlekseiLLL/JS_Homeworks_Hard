2d27dd8f29b97fef46e1737d5a125b2a73be6878
Aleksei <aleksei.lokhno@ukr.net>
2018-04-11 21:39:34 +0300

    Homework 12 Hard ver.1.1

--- lesson-12/js/script.js
+++ lesson-12/js/script.js
@@ -29,4 +29,30 @@ jQuery( document ).ready( function( $ ) {
 			backgroundImage: 'linear-gradient(45deg, #ed1c24, #fcee21)'
 		}, 1500);
 	});
+
+// Отправка данных с формы на сервер
+	$( function() {
+		$(' .contactform_select' ).submit( function( event ) {
+    		// Отмена стандартного события отправки данных
+    		event.preventDefault();
+    		// Получаем форму
+    		let form = $( this );
+
+    		$.ajax({
+    			// Указываем, как отправлять данные формы
+    			type: form.attr( 'method' ),
+    			// Данные формы отправляются на страницу, указанную в атрибуте действия
+    			url: form.attr( 'action' ),
+    			// Добавляет данные формы внутри тела HTTP-запроса
+    			data: form.serialize()
+    			// Если отправка успешная
+    		}).done( function( data ) {
+      			alert( 'Отправка данных прошла успешно!' );
+      			// Если отправка неудачная 
+  			}).fail( function( data ) {
+      			alert( 'Что-то пошло не так... Попробуйте снова через некоторое время.' );
+  			});
+		});
+	});
+
 });
